d01d1fc92fda00bc64f71d0a9b1409317bd83690
juman2312 <jumanhassan47@gmail.com>
2023-11-28 19:55:22 +0200

    l

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
-output = ""
-for i in range(10):
-    for j in range(i, 10):
-        if i != j:
-            output += str(i) + str(j) + ", "
-
-output = output[:-2]  # Remove the trailing comma and space
-print(output)
+for i in range(100):
+    print("{:02d}".format(i), end='')
+    if i != 99:
+        print(", ", end='')
