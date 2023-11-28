3e578dfb85cf713058cf80fef8bd71ed2ae2ef79
juman2312 <jumanhassan47@gmail.com>
2023-11-28 19:52:59 +0200

    l

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -4,7 +4,7 @@ output = ""
 for i in range(10):
     for j in range(i, 10):
         if i != j:
-            output += "{}{}, ".format(i, j)
+            output += str(i) + str(j) + ", "
 
 output = output[:-2]  # Remove the trailing comma and space
 print(output)
