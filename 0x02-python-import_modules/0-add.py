d9a1c99b44766abb8f3b384300673e60e57cf2db
juman2312 <jumanhassan47@gmail.com>
2023-11-30 22:14:56 +0200

    l

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-from operator import add
-import os
-
-a = 1
-b = 2
-
-add_file_path = os.path.join(os.path.dirname(__file__), 'add_0.py')
-exec(open(add_file_path).read())
-
-result = add(a, b)
-
-print("{} + {} = {}".format(a, b, result))
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
