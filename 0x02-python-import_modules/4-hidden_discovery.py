90d025a001fa082b03efbf812b4bea395f7c9e43
juman2312 <jumanhassan47@gmail.com>
2023-11-30 23:41:22 +0200

    l

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import marshal
+
+def print_hidden_names():
+    with open('hidden_4.pyc', 'rb') as file:
+        code = marshal.load(file)
+
+    names = set()
+    for const in code.co_consts:
+        if isinstance(const, str) and not const.startswith('__'):
+            names.add(const)
+
+    for name in sorted(names):
+        print(name)
+
+if __name__ == "__main__":
+    print_hidden_names()
